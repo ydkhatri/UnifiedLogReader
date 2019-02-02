818299193eb619c7daf2d7234e4f6761dea9d746
Joachim Metz <joachim.metz@gmail.com>
2019-02-02 12:12:11 +0100

    Refactored VirtualFileSystem to stand-alone file and added tests

--- UnifiedLog/Lib.py
+++ UnifiedLog/Lib.py
@@ -58,37 +58,6 @@ from UnifiedLog import logger
 
 # Timesync in-memory and persist start values not found in Tracev3
 
-class VirtualFileSystem(object):
-    '''
-        This class implements the file system functions that the library relies on.
-        In this base class, they default to the local OS ones such as os.path.exits(),
-        os.listdir() and a few others. To make them do something else, inherit the 
-        class and override its methods.
-    '''
-    def __init__(self, virtual_file_class):
-        super(VirtualFileSystem, self).__init__()
-        self.virtual_file_class = virtual_file_class
-    
-    def path_exists(self, path):
-        '''Return True if file/folder specified by 'path' exists'''
-        return os.path.exists(path)
-    
-    def listdir(self, path):
-        '''Return a list of all files/folders contained at given path'''
-        return os.listdir(path)
-
-    def is_dir(self, path):
-        '''Return True if path is a directory'''
-        return os.path.isdir(path)
-
-    def path_join(self, path, *paths):
-        '''Return the joined path, similar to os.path.join(path, *paths)'''
-        return os.path.join(path, *paths)
-
-    def get_virtual_file(self, path, filetype=''):
-        '''Return a VirtualFile object'''
-        return self.virtual_file_class(path, filetype)
-
 def ReadAPFSTime(mac_apfs_time): # Mac APFS timestamp is nano second time epoch beginning 1970/1/1
     '''Returns datetime object, or empty string upon error'''
     if mac_apfs_time not in ( 0, None, ''):
---
+++ tests/virtual_file_system.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''Tests for the virtual file system.'''
+
+from __future__ import unicode_literals
+
+import os
+import unittest
+
+from UnifiedLog import virtual_file
+from UnifiedLog import virtual_file_system
+
+from tests import test_lib
+
+
+class VirtualFileSystemTests(test_lib.BaseTestCase):
+    '''Tests for the virtual file system.'''
+
+    def testPathExists(self):
+        '''Tests the path_exists function.'''
+        file_system = virtual_file_system.VirtualFileSystem(
+            virtual_file.VirtualFile)
+
+        path = os.path.join(self._TEST_DATA_PATH, 'LICENSE.txt')
+        result = file_system.path_exists(path)
+        self.assertTrue(result)
+
+        path = os.path.join(self._TEST_DATA_PATH, 'bogus')
+        result = file_system.path_exists(path)
+        self.assertFalse(result)
+
+    def testListdir(self):
+        '''Tests the listdir function.'''
+        file_system = virtual_file_system.VirtualFileSystem(
+            virtual_file.VirtualFile)
+
+        directory_entries = file_system.listdir(self._TEST_DATA_PATH)
+        self.assertEqual(len(directory_entries), 1)
+        self.assertEqual(directory_entries, ['LICENSE.txt'])
+
+    def testIsDir(self):
+        '''Tests the is_dir function.'''
+        file_system = virtual_file_system.VirtualFileSystem(
+            virtual_file.VirtualFile)
+
+        result = file_system.is_dir(self._TEST_DATA_PATH)
+        self.assertTrue(result)
+
+        path = os.path.join(self._TEST_DATA_PATH, 'LICENSE.txt')
+        result = file_system.is_dir(path)
+        self.assertFalse(result)
+
+    def testPathJoin(self):
+        '''Tests the path_join function.'''
+        file_system = virtual_file_system.VirtualFileSystem(
+            virtual_file.VirtualFile)
+
+        expected_path = os.path.join(self._TEST_DATA_PATH, 'LICENSE.txt')
+        path = file_system.path_join(self._TEST_DATA_PATH, 'LICENSE.txt')
+        self.assertEqual(path, expected_path)
+
+    def testGetVirtualFile(self):
+        '''Tests the get_virtual_file function.'''
+        file_system = virtual_file_system.VirtualFileSystem(
+            virtual_file.VirtualFile)
+
+        path = os.path.join(self._TEST_DATA_PATH, 'LICENSE.txt')
+        file_object = file_system.get_virtual_file(path, filetype='test')
+        self.assertIsNotNone(file_object)
+        self.assertIsInstance(file_object, virtual_file.VirtualFile)
+
+
+if __name__ == '__main__':
+    unittest.main()

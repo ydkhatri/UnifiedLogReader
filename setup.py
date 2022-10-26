e56334c458ef237ff10c06f0b9bee9513aabefe2
Ruud Schramp <schramp@holmes.nl>
2022-10-26 16:21:02 +0200

    Do not attempt to read requires.txt from setup.py

--- setup.py
+++ setup.py
@@ -11,13 +11,6 @@ try:
 except ImportError:
   from distutils.core import find_packages, setup
 
-try:  # for pip >= 10
-    from pip._internal.download import PipSession
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.download import PipSession
-    from pip.req import parse_requirements
-
 # Change PYTHONPATH to include UnifiedLog so that we can get the version.
 sys.path.insert(0, '.')
 
@@ -57,7 +50,4 @@ setup(
         ('share/doc/UnifiedLog', [
             'LICENSE.txt', 'README.md']),
     ],
-    install_requires=[str(req.req) for req in parse_requirements(
-        'requirements.txt', session=PipSession(),
-    )],
 )

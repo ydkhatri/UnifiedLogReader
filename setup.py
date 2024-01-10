2cb6b151b21fce915ae5b805b518186746a87758
Julia Paluch <julia.paluch@strozfriedberg.com>
2024-01-10 17:08:37 -0500

    update setup.py to work with new version of pip. enforce pip > 23.0

--- setup.py
+++ setup.py
@@ -11,12 +11,8 @@ try:
 except ImportError:
   from distutils.core import find_packages, setup
 
-try:  # for pip >= 10
-    from pip._internal.download import PipSession
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.download import PipSession
-    from pip.req import parse_requirements
+from pip._internal.network.session import PipSession
+from pip._internal.req.req_file import parse_requirements
 
 # Change PYTHONPATH to include UnifiedLog so that we can get the version.
 sys.path.insert(0, '.')
@@ -57,7 +53,7 @@ setup(
         ('share/doc/UnifiedLog', [
             'LICENSE.txt', 'README.md']),
     ],
-    install_requires=[str(req.req) for req in parse_requirements(
+    install_requires=[req.requirement for req in parse_requirements(
         'requirements.txt', session=PipSession(),
     )],
 )
